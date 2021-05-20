77ce06b48e329afbb262c6c9eaae9fc3fd522eef
resourceldg <resourceldg@gmail.com>
2021-05-20 02:30:31 -0300

    tiny vivi  photos

--- frontend/src/routes/collections.svelte
+++ frontend/src/routes/collections.svelte
@@ -1,9 +1,9 @@
 <section class="text-gray-600 body-font">
     <div class="container mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
-        <h2 class="text-xs text-red-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+        <h2 class="text-xs text-red-500 tracking-widest font-medium title-font mb-1">COLLECIONES</h2>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Universo Jarilla</h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Nuestra trayectoria, con lo mejor de nuestros autores.</p>
       </div>
       <div class="flex flex-wrap">
         <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
